c260bd0aac95ad9e64559af906c4caec056c5458
yoyhyu <66709690+yoyhyu@users.noreply.github.com>
2022-01-27 17:53:41 +0900

    데스크탑용 FPS 게임

---
+++ FpsGame/Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerFire : MonoBehaviour
+{
+    public GameObject firePosition;
+    public GameObject bombFactory;
+    public float throwPower = 15f;
+
+    public GameObject bulletEffect;
+    ParticleSystem ps;
+
+    public int weaponPower = 5;
+
+    Animator anim;
+
+    enum WeaponMode
+    {
+        Normal,
+        Sniper
+    }
+    WeaponMode wMode;
+
+    bool ZoomMode = false;
+
+    public Text wModeText;
+
+    public GameObject[] eff_Flash;
+
+    public GameObject weapon01;
+    public GameObject weapon02;
+
+    public GameObject crosshair01;
+    public GameObject crosshair02;
+
+    public GameObject weapon01_R;
+    public GameObject weapon02_R;
+
+    public GameObject crosshair02_zoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = bulletEffect.GetComponent<ParticleSystem>();
+
+        anim = GetComponentInChildren<Animator>();
+
+        wMode = WeaponMode.Normal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.gm.gState != GameManager.GameState.Run)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            switch(wMode)
+            {
+                case WeaponMode.Normal:
+                    GameObject bomb = Instantiate(bombFactory);
+                    bomb.transform.position = firePosition.transform.position;
+
+                    Rigidbody rb = bomb.GetComponent<Rigidbody>();
+
+                    rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+                    break;
+                case WeaponMode.Sniper:
+                    if(!ZoomMode)
+                    {
+                        Camera.main.fieldOfView = 15f;
+                        ZoomMode = true;
+
+                        crosshair02_zoom.SetActive(true);
+                        crosshair02.SetActive(false);
+                    }
+                    else
+                    {
+                        Camera.main.fieldOfView = 60f;
+                        ZoomMode = false;
+
+                        crosshair02_zoom.SetActive(false);
+                        crosshair02.SetActive(true);
+                    }
+                    break;
+            }
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(anim.GetFloat("MoveMotion") == 0)
+            {
+                anim.SetTrigger("Attack");
+            }
+
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            RaycastHit hitInfo = new RaycastHit();
+
+            if(Physics.Raycast(ray, out hitInfo))
+            {
+                if(hitInfo.transform.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+                {
+                    EnemyFSM eFSM = hitInfo.transform.GetComponent<EnemyFSM>();
+                    eFSM.HitEnemy(weaponPower);
+                }
+                else
+                {
+                    bulletEffect.transform.position = hitInfo.point;
+                    bulletEffect.transform.forward = hitInfo.normal;
+                    ps.Play();
+                }
+            }
+
+            StartCoroutine(ShootEffectOn(0.05f));
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            wModeText.text = "Normal Mode";
+
+            weapon01.SetActive(true);
+            weapon02.SetActive(false);
+            crosshair01.SetActive(true);
+            crosshair02.SetActive(false);
+
+            weapon01_R.SetActive(true);
+            weapon02_R.SetActive(false);
+
+            crosshair02_zoom.SetActive(false);
+
+            wMode = WeaponMode.Normal;
+            Camera.main.fieldOfView = 60f;
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            wModeText.text = "Sniper Mode";
+
+            weapon01.SetActive(false);
+            weapon02.SetActive(true);
+            crosshair01.SetActive(false);
+            crosshair02.SetActive(true);
+
+            weapon01_R.SetActive(false);
+            weapon02_R.SetActive(true);
+
+            wMode = WeaponMode.Sniper;
+        }
+    }
+
+    IEnumerator ShootEffectOn(float duration)
+    {
+        int num = Random.Range(0, eff_Flash.Length);
+        eff_Flash[num].SetActive(true);
+        yield return new WaitForSeconds(duration);
+
+        eff_Flash[num].SetActive(false);
+    }
+}
---
+++ FpsGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 7f;
+
+    CharacterController cc;
+    float gravity = -20f;
+    float yVelocity = 0;
+
+    public float jumpPower = 10f;
+
+    public bool isJumping = false;
+
+    public int hp = 20;
+    int maxHp = 20;
+
+    public Slider hpSlider;
+
+    public GameObject hitEffect;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.gm.gState != GameManager.GameState.Run)
+        {
+            return;
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        dir = dir.normalized;
+
+        anim.SetFloat("MoveMotion", dir.magnitude);
+
+        dir = Camera.main.transform.TransformDirection(dir);
+
+        if(cc.collisionFlags == CollisionFlags.Below)
+        {
+            if(isJumping)
+            {
+                isJumping = false;
+                yVelocity = 0;
+            }
+        }
+
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            yVelocity = jumpPower;
+            isJumping = true;
+        }
+
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+
+        cc.Move(dir * moveSpeed * Time.deltaTime);
+
+        hpSlider.value = (float)hp / (float)maxHp;
+    }
+
+    public void DamageAction(int damage)
+    {
+        hp -= damage;
+
+        if (hp > 0)
+        {
+            StartCoroutine(PlayHitEffect());
+        }
+    }
+
+    IEnumerator PlayHitEffect()
+    {
+        hitEffect.SetActive(true);
+        yield return new WaitForSeconds(0.3f);
+        hitEffect.SetActive(false);
+    }
+}
